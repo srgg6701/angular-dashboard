11bcad3cbbe653e6f4490184e1110367807fca50
srgg6701 <srgg67@gmail.com>
2016-03-17 11:42:21 +0300

    Удаление тестового кода.

--- app/scripts/controllers/defaultCtrl.js
+++ app/scripts/controllers/defaultCtrl.js
@@ -1,7 +1,7 @@
 // strict mode coming later
 app.controller('defaultCtrl', function ($scope, DashboardContents, DashboardActions) {
     DashboardContents.getDashboardContents().then(function(data){
-        $scope.dashboard = data; //console.log(data);
+        $scope.dashboard = data;
     });
 
     $scope.newtask={};
@@ -9,34 +9,12 @@ app.controller('defaultCtrl', function ($scope, DashboardContents, DashboardActi
     $scope.newcategory={};
 
     $scope.relocateCard = function(scope, statuses){ //card
-        //alert('relocate!');
+        //
         var columnScope = $scope.dashboard.columns,
             old_status=statuses[0], new_status=statuses[1],
             oldScopeCardBox=columnScope[old_status][1], newScopeCardBox=columnScope[new_status][1];
-        console.groupCollapsed('%ccard', 'font-size:14px; background-color:yellow; padding:3px 6px; border-radius:3px;');
-        console.log({
-            '0 old status': old_status,
-            '1 new status': new_status,
-            '2 card': {
-                '0 index':scope.$index,
-                '1 issue':scope.issue
-            },
-            '3 card scope': scope,
-            '4 data': {
-                '1 old scope (cards box)': oldScopeCardBox,
-                '1 new scope (cards box)': newScopeCardBox,
-                '2 length': columnScope[new_status].length-1,
-                '3 remove from old scope': oldScopeCardBox[scope.$index]
-            },
-            '5 $scope': $scope
-        });
-        console.log('oldScopeCardBox before', {'1':oldScopeCardBox, '2':columnScope[old_status]});
-
-        //var cardScope =
         oldScopeCardBox.splice(scope.$index,1);
         newScopeCardBox.push(scope.issue);
-
-        console.groupEnd();
 
         $scope.$apply();
 
@@ -45,35 +23,17 @@ app.controller('defaultCtrl', function ($scope, DashboardContents, DashboardActi
     $scope.imposeCard = function(cardScope){
         var index = cardScope.$index,
             parentData = cardScope.$parent.data[1];
-        console.log({
-            'index':index,
-            'cardScope':cardScope,
-            'parentData1': parentData
-        });
         parentData.splice(index,1);
         $scope.$apply();
-        console.log('scope', $scope);
     };
 
     $scope.removeClone = function(panel, cardId, sectionId){
-        console.log('removeClone', arguments);
+
         var clonesArray=$scope.dashboard.panels[panel.status][1],
             cardNativeId = cardId.substring(4, cardId.indexOf("_")),
             cloneIndex=clonesArray.indexOf(cardNativeId);
-        console.log({
-            panels: $scope.dashboard.panels,
-            status: panel.status,
-            panelData: $scope.dashboard.panels[panel.status],
-            dataArray: $scope.dashboard.panels[panel.status][1],
-            scopeSegment: clonesArray
-        });
 
         var removed=$scope.dashboard.panels[panel.status][1].splice(cloneIndex,1);
-
-        // console.log('panelData', {panelData:$scope.dashboard.panels[panel.status], removed:removed});
-        // 1: "task4_0_"
-        // 2: "box-rows-0"
-
 
         $scope.$apply();
     };
@@ -83,12 +43,6 @@ app.controller('defaultCtrl', function ($scope, DashboardContents, DashboardActi
     };
     $scope.removePanel = function(status){
         DashboardActions.removePanel($scope, status);
-    };
-
-    $scope.removeCard = function(event){ // cardId, status, index
-        console.trace();
-        console.log('event', event); //, $scope
-        //DashboardActions.removeCard($scope, cardId, status, index);
     };
 
     $scope.addTask = function(){
--- app/scripts/services/dashboardActions.js
+++ app/scripts/services/dashboardActions.js
@@ -1,18 +1,11 @@
 app.service('DashboardActions', function($http, DashboardContents){
 
     this.removeColumn = function(scope, status){
-        delete scope['dashboard'].columns[status]; //console.log('removeColumn', status, $scope.dashboard.columns);
+        delete scope['dashboard'].columns[status];
     };
     this.removePanel = function(scope, status){
-        delete scope['dashboard'].panels[status]; //console.log('removePanel', status, $scope.dashboard.panels);
+        delete scope['dashboard'].panels[status];
     };
-    this.removeCard = function(scope, cardId, status, index){
-        //console.log({cardId: cardId, scope:scope, status:status, index:index});
-        console.log('removeCard before', { path: 'scope.dashboard.columns['+status+'][1].splice('+index+',1)', object: scope.dashboard.columns[status][1], columns: scope.dashboard.columns});
-        var removed=scope.dashboard.columns[status][1].splice(index,1);
-        console.log('removeCard after', { columns: scope.dashboard.columns, removed: removed });
-    };
-
     function getNewLastInsertId(scope){
         return scope.dashboard.lastId+1;
     }
@@ -22,7 +15,6 @@ app.service('DashboardActions', function($http, DashboardContents){
         /** добавляется всегда, аналогично вставке новой записи в SQL-таблицу;
          *  нужно для назначения корректного task.id  */
         if(!lastInsertId) dashboard.lastId = getNewLastInsertId(scope);
-        console.log('%csave', 'color: green', dashboard);
         $http.post('app/data/dashboard', dashboard).then( function(){
             scope.newtask.show=false;
         });
@@ -32,7 +24,7 @@ app.service('DashboardActions', function($http, DashboardContents){
         try{
             var lastInsertId = getNewLastInsertId(scope),
             dataToPush=[ lastInsertId, scope.newtask.text ];
-            console.log('add task', { lastInsertId:lastInsertId, push_into: scope.dashboard.columns.new[1], dataToPush:dataToPush});
+
             scope.dashboard.columns.new[1].push(dataToPush);
             saveData(scope, lastInsertId);
         }catch(e){
@@ -40,8 +32,7 @@ app.service('DashboardActions', function($http, DashboardContents){
         }
     };
     this.addGroup = function(scope){ //.newgroup
-        console.log('addGroup, scope', scope);
-        //var pn={}; //pn[data.class]=[data.name, [data.color]];
+
         try{
             var newgroup = scope.newgroup;
             scope.dashboard.columns[newgroup.class]=[newgroup.name, [], [newgroup.color]];
@@ -51,7 +42,7 @@ app.service('DashboardActions', function($http, DashboardContents){
         }
     };
     this.addPanel = function(scope){
-        console.log('add panel, scope', scope);
+
         try{
             var newcategory = scope.newcategory;
             scope.dashboard.panels[newcategory.class]=[newcategory.name, [newcategory.color]];
